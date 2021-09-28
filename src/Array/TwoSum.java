1225a936a3bf4891e7295953d315da07f6232acb
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-09-28 20:07:01 +0530

    added DP problems

--- src/Array/TwoSum.java
+++ src/Array/TwoSum.java
@@ -1,6 +1,8 @@
 package Array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.stream.IntStream;
 
 public class TwoSum {
@@ -8,14 +10,16 @@ public class TwoSum {
         int [] array = {5,2,1,3};
         int target = 3 ;
         int [] num = new int[2];
-        two(array,target,0,1,num);
-        for(int n : num){
-            System.out.println(n);
-        }
-        Arrays.sort(array);
 
-        System.out.println(Arrays.stream(array).max().getAsInt());
-        System.out.println(Arrays.binarySearch(array,312));
+        twoSum(array,target);
+//        two(array,target,0,1,num);
+//        for(int n : num){
+//            System.out.println(n);
+//        }
+//        Arrays.sort(array);
+//
+//        System.out.println(Arrays.stream(array).max().getAsInt());
+//        System.out.println(Arrays.binarySearch(array,312));
     }
 
     private static int [] two(int [] array , int target, int start , int end , int [] num){
@@ -29,6 +33,21 @@ public class TwoSum {
             }
         }
         return num;
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i++){
+            int complement = target - nums[i];
+
+            if(hashMap.containsKey(complement)){
+                return new int[]{hashMap.get(complement),i};
+            }
+
+            hashMap.put(nums[i],i);
+        }
+        return new int[]{};
     }
 }
 
--- src/DynamicProg/FiboNumber.java
+++ src/DynamicProg/FiboNumber.java
@@ -1,23 +1,29 @@
 package DynamicProg;
+
+import java.util.Arrays;
+
 // 0 1 1 2 3 5 8 13
 public class FiboNumber {
     public static void main(String[] args) {
-        System.out.println(fib(3));
-    }
-    private static int fib(int n) {
-        return fiboRec(0,1,n);
-    }
-
-    private static int fiboRec(int first , int second , int numberOfTimesLoop ){
-        if (numberOfTimesLoop > 0){
-            int newSecond = first + second ;
-            numberOfTimesLoop--;
-            return fiboRec(second , newSecond ,numberOfTimesLoop);
+        int n = 8;
+        int[] memo = new int[n+1];
+        for(int i = 0 ; i < n ; i++){
+            memo[i]=-1;
         }
-        return first;
+        System.out.println(fiboDp(5,memo));
+        System.out.println(Arrays.toString(memo));
     }
 
-    private static void fiboDp(int n,int[] memo){
 
+    private static int fiboDp(int n,int[] memo){
+        if(memo[n]==-1){
+            int res=0;
+            if(n==0||n==1)
+                res=n;
+            else
+                res=fiboDp(n-1,memo)+fiboDp(n-2,memo);
+            memo[n]=res;
+        }
+        return memo[n];
     }
 }
